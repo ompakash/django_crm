3fe354246846cae4dedf281d0df23f73369a433e
omprakash <opcoc3@gmail.com>
2021-08-17 22:14:15 +0530

    templates initial

--- djcrm/leads/views.py
+++ djcrm/leads/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse("Hello World")
+    leads = Lead.objects.all()
+    context = {
+        "leads":leads,
+    }
+    return render(request, 'leads/homepage.html',context)
