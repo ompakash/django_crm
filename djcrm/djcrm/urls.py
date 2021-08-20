fe481cae4863703626e1ea7a61d0a81ae55d48ce
omprakash <opcoc3@gmail.com>
2021-08-20 08:01:32 +0530

    url namespace added

--- djcrm/djcrm/urls.py
+++ djcrm/djcrm/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls'), name="leads"),
+    path('leads/', include('leads.urls', namespace="leads")),
 ]
--- djcrm/leads/urls.py
+++ djcrm/leads/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
+from django.utils.regex_helper import normalize
 from .views import lead_create, lead_delete, lead_list,lead_detail,lead_update
 
-# app_name = "leads"
+app_name = "leads"
 
 urlpatterns = [
-    path('',lead_list), 
-    path('<int:pk>/',lead_detail),
-    path('<int:pk>/update/',lead_update),
-    path('<int:pk>/delete/',lead_delete),
-    path('create/',lead_create),
+    path('',lead_list,name="lead-list"), 
+    path('<int:pk>/',lead_detail,name="lead-detail"),
+    path('<int:pk>/update/',lead_update,name="lead-update"),
+    path('<int:pk>/delete/',lead_delete,name="lead-delete"),
+    path('create/',lead_create,name="lead-create"),
     
 
 ]
