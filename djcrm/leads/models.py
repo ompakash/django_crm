08af569c8455d8e02ae4a6446fd2155a7f97f661
ompakash <opytc2@gmail.com>
2021-09-13 21:22:26 +0530

    added signal

--- djcrm/leads/models.py
+++ djcrm/leads/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.db.models.signals import post_save,pre_save
 from django.contrib.auth.models import AbstractUser
+
 
 
 # Create your models here.
@@ -12,9 +14,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
-
-    
+        return self.user.username  
 
 class Lead(models.Model):
     
@@ -34,3 +34,9 @@ class Agent(models.Model):
     def __str__(self):
         return self.user.username
     
+def post_user_created_signal(sender,instance,created, **kwargs):
+    print(instance,created)
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal,sender=User)
